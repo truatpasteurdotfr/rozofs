f91ee61766ef04a5cdda65491976d939b0f2da00
rozocaffeine <pierre.evenou@rozofs.org>
2012-06-04 16:22:01 +0200

    added econfig[h.c].

---
+++ src/econfig.h
@@ -0,0 +1,76 @@
+/*
+  Copyright (c) 2010 Fizians SAS. <http://www.fizians.com>
+  This file is part of Rozofs.
+
+  Rozofs is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published
+  by the Free Software Foundation; either version 3 of the License,
+  or (at your option) any later version.
+
+  Rozofs is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see
+  <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _ECONFIG_H
+#define _ECONFIG_H
+
+#include <stdio.h>
+#include <limits.h>
+#include "rozofs.h"
+#include "list.h"
+
+#define MD5_LEN  22
+
+typedef struct storage_node_config {
+    sid_t sid;
+    char host[ROZOFS_HOSTNAME_MAX];
+    list_t list;
+} storage_node_config_t;
+
+typedef struct cluster_config {
+    cid_t cid;
+    list_t storages;
+    list_t list;
+} cluster_config_t;
+
+typedef struct volume_config {
+    vid_t vid;
+    list_t clusters;
+    list_t list;
+} volume_config_t;
+
+typedef struct export_config {
+	eid_t eid;
+	vid_t vid;
+	char root[FILENAME_MAX];
+	char md5[MD5_LEN];
+	uint64_t squota;
+	uint64_t hquota;
+	list_t list;
+} export_config_t;
+
+typedef struct econfig {
+	int layout;
+	list_t volumes;
+	list_t exports;
+} econfig_t;
+
+int econfig_initialize(econfig_t *config);
+
+void econfig_release(econfig_t *config);
+
+int econfig_read(econfig_t *config, const char *fname);
+
+int econfig_validate(econfig_t *config);
+
+int econfig_check_consistency(econfig_t *from, econfig_t *to);
+
+int econfig_print(econfig_t *config);
+
+#endif
