117a003038c8b049048c60a45f9ee0b009ce85f8
rozonicotine <didier.feron@rozofs.org>
2013-04-04 11:34:49 +0200

    Add slice(256) and subslice(4096) support in exportd

--- src/exportd/export.c
+++ src/exportd/export.c
@@ -79,40 +79,136 @@ typedef struct cnxentry {
 
 DECLARE_PROFILING(epp_profiler_t);
 
+
+/*
+**__________________________________________________________________
+*/
 /** get the lv1 directory.
  *
  * lv1 entries are first level directories of an export root named by uint32_t
  * string value and used has entry of a hash table storing the export
  * meta data files.
  *
- * @param export: the export used to find directory
- * @param fid: the search fid
- * @param create: whenever directory will be created or not
+ * @param root_path: root path of the exportd
+ * @param slice: value of the slice
  *
- * @return the entry on success otherwise -1
+ * @return 0 on success otherwise -1
  */
-static int export_lv1_resolve_entry(export_t *export, fid_t fid, int create) {
-    uint32_t hash = 0;
-    uint8_t *c = 0;
+static inline int mstor_slice_resolve_entry(char *root_path,uint32_t slice) {
     char path[PATH_MAX];
-    START_PROFILING(export_lv1_resolve_entry);
-
-    for (c = fid; c != fid + 16; c++)
-        hash = *c + (hash << 6) + (hash << 16) - hash;
-    hash %= MAX_LV1_BUCKETS;
-    sprintf(path, "%s/%"PRIu32"", export->root, hash);
+    sprintf(path, "%s/%"PRId32"", root_path, slice);
     if (access(path, F_OK) == -1) {
-        if (errno == ENOENT && create) {
-            if (mkdir(path, S_IRUSR | S_IWUSR | S_IXUSR) != 0)
-                return -1;
-        } else {
+        if (errno == ENOENT) 
+        {
+          /*
+          ** it is the fisrt time we access to the slice
+          **  we need to create the level 1 directory and the 
+          ** timestamp file
+          */
+          if (mkdir(path, S_IRUSR | S_IWUSR | S_IXUSR) != 0)
+          {
+            severe("mkdir (%s): %s",path,strerror(errno));
             return -1;
-        }
+          }                
+//          mstor_ts_srv_create_slice_timestamp_file(export,slice); 
+          return 0;         
+        } 
+        /*
+        ** any other error
+        */
+        severe("access (%s): %s",path,strerror(errno));
+        return -1;
     }
-
-    STOP_PROFILING(export_lv1_resolve_entry);
-    return hash;
+    return 0;
 }
+
+
+/*
+**__________________________________________________________________
+*/
+/** get the subslice directory index.
+ *
+ * lv1 entries are first level directories of an export root named by uint32_t
+ * string value and used has entry of a hash table storing the export
+ * meta data files.
+ *
+ * @param root_path: root path of the exportd
+ * @param fid: the search fid
+ *
+ * @return 0 on success otherwise -1
+ */
+static inline int mstor_subslice_resolve_entry(char *root_path, fid_t fid,uint32_t slice,uint32_t subslice) 
+{
+    char path[PATH_MAX];
+
+
+    sprintf(path, "%s/%d/%d",root_path, slice,subslice);
+    if (access(path, F_OK) == -1) {
+        if (errno == ENOENT) 
+        {
+          /*
+          ** it is the fisrt time we access to the subslice
+          **  we need to create the associated directory 
+          */
+          if (mkdir(path, S_IRUSR | S_IWUSR | S_IXUSR) != 0)
+          {
+            severe("mkdir (%s): %s",path,strerror(errno));
+            return -1;
+          }
+          return 0;
+        } 
+        severe("access (%s): %s",path,strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+/** build a full path based on export root and fid of the lv2 file
+ *
+ * lv2 is the second level of files or directories in storage of metadata
+ * they are acceded thru mreg or mdir API according to their type.
+ *
+ * @param root_path: root path of the exportd
+ * @param fid: the fid we are looking for
+ * @param path: the path to fill in
+ */
+
+static inline int export_lv2_resolve_path_internal(char *root_path, fid_t fid, char *path) 
+{
+    uint32_t slice;
+    uint32_t subslice;
+    char str[37];
+    
+    /*
+    ** extract the slice and subsclie from the fid
+    */
+    mstor_get_slice_and_subslice(fid,&slice,&subslice);
+    /*
+    ** check the existence of the slice directory: create it if it does not exist
+    */
+    if (mstor_slice_resolve_entry(root_path,slice) < 0)
+    {
+        goto error;
+    }
+    /*
+    ** check the existence of the subslice directory: create it if it does not exist
+    */
+    if (mstor_subslice_resolve_entry(root_path, fid,slice,subslice) < 0)
+    {
+        goto error;
+    }
+    /*
+    ** convert the fid in ascii
+    */
+    uuid_unparse(fid, str);
+    sprintf(path, "%s/%d/%d/%s", root_path, slice,subslice,str);
+    return 0;
+    
+error:
+    return -1;
+}
+
+
 
 /** build a full path based on export root and fid of the lv2 file
  *
@@ -123,20 +219,22 @@ static int export_lv1_resolve_entry(export_t *export, fid_t fid, int create) {
  * @param fid: the fid we are looking for
  * @param path: the path to fill in
  */
-static int export_lv2_resolve_path(export_t *export, fid_t fid, char *path) {
-    int lv1 = -1;
-    char str[37];
+
+int export_lv2_resolve_path(export_t *export, fid_t fid, char *path) 
+{
+    int ret;
+
     START_PROFILING(export_lv2_resolve_path);
-
-    if ((lv1 = export_lv1_resolve_entry(export, fid, LV1_CREATE)) < 0)
-        return -1;
-
-    uuid_unparse(fid, str);
-    sprintf(path, "%s/%d/%s", export->root, lv1, str);
+    
+    ret = export_lv2_resolve_path_internal(export->root,fid,path);
 
     STOP_PROFILING(export_lv2_resolve_path);
-    return 0;
+    return ret;
 }
+
+
+
+
 
 /** search a fid in the cache
  *
@@ -539,20 +637,16 @@ int export_create(const char *root) {
     if ((root_attrs.ctime = root_attrs.atime = root_attrs.mtime = time(NULL)) == -1)
         return -1;
     root_attrs.size = ROZOFS_DIR_SIZE;
-
-    // create the lv1 directory
-    for (c = root_attrs.fid; c != root_attrs.fid + 16; c++)
-        lv1 = *c + (lv1 << 6) + (lv1 << 16) - lv1;
-    lv1 %= MAX_LV1_BUCKETS;
-    sprintf(root_path, "%s/%u", path, lv1);
-    if (mkdir(root_path, S_IRUSR | S_IWUSR | S_IXUSR) != 0)
+    /*
+    ** create the slice and subslice directory for root if they don't exist
+    ** and then create the "fid" directory or the root
+    */
+    if (export_lv2_resolve_path_internal(path,root_attrs.fid,root_path) != 0)
         return -1;
-
-    // create the lv2 directory
-    uuid_unparse(root_attrs.fid, fidstr);
-    sprintf(root_path, "%s/%u/%s", root, lv1, fidstr);
-    if (mkdir(root_path, S_IRUSR | S_IWUSR | S_IXUSR) != 0)
+        
+    if (mkdir(root_path, S_IRUSR | S_IWUSR | S_IXUSR) < 0) {
         return -1;
+    }
 
     // open the root mdir
     if (mdir_open(&root_mdir, root_path) != 0) {
