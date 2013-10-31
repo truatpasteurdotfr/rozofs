73e64109d3815ded4873a7145fa99cd4a575e3d8
Dimitri Pertin <dimitri.pertin@univ-nantes.fr>
2013-10-31 11:36:30 +0100

    Issue #48 - Add 'rozo node config'
    'rozo node config' display the system configuration
    Once two volumes/exports holding each 4 storages and storaged listens to
    3 interfaces, here's what it gives:
    
    ~~~~
    rozo node config -E localhost'NODE: localhost':
    - EXPORTD:
      - VOLUME:
        - volume 1:
          - cluster 1:
            - {sid 1: localhost}
            - {sid 2: localhost}
            - {sid 3: localhost}
            - {sid 4: localhost}
        - volume 2:
          - cluster 2:
            - {sid 1: localhost}
            - {sid 2: localhost}
            - {sid 3: localhost}
            - {sid 4: localhost}
      - EXPORT:
          vid: 1
          root: /srv/rozofs/exports/export_1
          md5: ''
          squota: ''
          hquota: ''
      - EXPORT:
          vid: 2
          root: /srv/rozofs/exports/export_2
          md5: ''
          squota: ''
          hquota: ''
    - STORAGED:
      - INTERFACE:
        - {192.168.0.1: 41001}
        - {192.168.0.2: 41001}
        - {192.168.0.3: 41001}
      - STORAGE:
        - {'cid 1, sid 1': /srv/rozofs/storages/storage_1_1}
        - {'cid 1, sid 2': /srv/rozofs/storages/storage_1_2}
        - {'cid 1, sid 3': /srv/rozofs/storages/storage_1_3}
        - {'cid 1, sid 4': /srv/rozofs/storages/storage_1_4}
        - {'cid 2, sid 1': /srv/rozofs/storages/storage_2_1}
        - {'cid 2, sid 2': /srv/rozofs/storages/storage_2_2}
        - {'cid 2, sid 3': /srv/rozofs/storages/storage_2_3}
        - {'cid 2, sid 4': /srv/rozofs/storages/storage_2_4}
    - ROZOFSMOUNT:
      - {node localhost: /srv/rozofs/exports/export_1}
      - {node localhost: /srv/rozofs/exports/export_2}
    ~~~~

--- manager/rozofs/cli/node.py
+++ manager/rozofs/cli/node.py
@@ -19,21 +19,23 @@
 import sys
 from rozofs.core.platform import Platform, Role
 from rozofs.core.agent import ServiceStatus
+from rozofs.core.constants import STORAGED_MANAGER, EXPORTD_MANAGER, \
+ROZOFSMOUNT_MANAGER
 import json
 from rozofs.cli.output import ordered_puts
 from collections import OrderedDict
 
-ROLES_STR = {Role.EXPORTD: "exportd", Role.STORAGED: "storaged", Role.ROZOFSMOUNT: "rozofsmount"}
+ROLES_STR = {Role.EXPORTD: "EXPORTD", Role.STORAGED: "STORAGED", Role.ROZOFSMOUNT: "ROZOFSMOUNT"}
 STR_ROLES = {"exportd": Role.EXPORTD, "storaged": Role.STORAGED, "rozofsmount": Role.ROZOFSMOUNT}
 
 def __roles_to_strings(roles):
     strs = []
     if roles & Role.EXPORTD == Role.EXPORTD:
-        strs.append("exportd")
+        strs.append("EXPORTD")
     if roles & Role.STORAGED == Role.STORAGED:
-        strs.append("storaged")
+        strs.append("STORAGED")
     if roles & Role.ROZOFSMOUNT == Role.ROZOFSMOUNT:
-        strs.append("rozofsmount")
+        strs.append("ROZOFSMOUNT")
     return strs
 
 
@@ -112,112 +114,130 @@ def start(platform, args):
 def stop(platform, args):
     platform.stop(args.nodes, __args_to_roles(args))
 
+def __exportd_config_to_string(config):
+    s = "\t\tLAYOUT: %d\n" % config.layout
+    for v in config.volumes.values():
+        s += "\t\tVOLUME: %d\n" % v.vid
+        for c in v.clusters.values():
+            s += "\t\t\tCLUSTER: %d\n" % c.cid
+            s += "\t\t\t\t%-20s %-10s\n" % ('NODE', 'SID')
+            for sid, h in c.storages.items():
+                s += "\t\t\t\t%-20s %-10d\n" % (h, sid)
+    if len(config.exports) != 0:
+        s += "\t\t%-4s %-4s %-25s %-25s %-10s %-10s\n" % ('EID', 'VID', 'ROOT', 'MD5', 'SQUOTA', 'HQUOTA')
+    for e in config.exports.values():
+        s += "\t\t%-4d %-4d %-25s %-25s %-10s %-10s\n" % (e.eid, e.vid, e.root, e.md5, e.squota, e.hquota)
 
-#
-# configuration related functions
-#
-# def __exportd_config_to_string(config):
-#    s = "\t\tLAYOUT: %d\n" % config.layout
-#    for v in config.volumes.values():
-#        s += "\t\tVOLUME: %d\n" % v.vid
-#        for c in v.clusters.values():
-#            s += "\t\t\tCLUSTER: %d\n" % c.cid
-#            s += "\t\t\t\t%-20s %-10s\n" % ('NODE', 'SID')
-#            for sid, h in c.storages.items():
-#                s += "\t\t\t\t%-20s %-10d\n" % (h, sid)
-#    if len(config.exports) != 0:
-#        s += "\t\t%-4s %-4s %-25s %-25s %-10s %-10s\n" % ('EID', 'VID', 'ROOT', 'MD5', 'SQUOTA', 'HQUOTA')
-#    for e in config.exports.values():
-#        s += "\t\t%-4d %-4d %-25s %-25s %-10s %-10s\n" % (e.eid, e.vid, e.root, e.md5, e.squota, e.hquota)
-#
-#    return s
-#
-#
-# def __storaged_config_to_string(config):
-#    # s = "\t\tLAYOUT: %d\n" % config.layout
-#    s = "\t\tPORTS: %s\n" % config.ports
-#    s += "\t\t%-10s %-10s %-30s\n" % ('CID', 'SID', 'ROOT')
-#    keylist = config.storages.keys()
-#    keylist.sort()
-#    for key in keylist:
-#        st = config.storages[key]
-#        s += "\t\t%-10d %-10d %-30s\n" % (st.cid, st.sid, st.root)
-#    return s
-#
-#
-# def __rozofsmount_config_to_string(config):
-#    # s = "\t\tPROTOCOLS: %s\n" % config.protocols
-#    s = "\t\t%-20s %-20s\n" % ('NODE', 'EXPORT')
-#    for c in config:
-#        s += "\t\t%-20s %-20s\n" % (c.export_host, c.export_path)
-#    return s
-#
-#
-# def __print_host_configs(host, configurations):
-#    if configurations is not None and not configurations:
-#        return
-#
-#    print >> sys.stdout, ":node:node status:roles:role statuses"
-#    if configurations is None:
-#        print >> sys.stdout, ":%s :%s:%s:%s" % (host, 'down', '', '')
-#        return
-#
-#    # __double_line()
-#    print >> sys.stdout, "NODE: %s - %s" % (host, 'UP')
-#    for r, c in configurations.items():
-#        # __single_line()
-#        print >> sys.stdout, "\tROLE: %s" % ROLES_STR[r]
-#        if (r & Role.EXPORTD == Role.EXPORTD):
-#            print >> sys.stdout, "%s" % __exportd_config_to_string(c)
-#        if (r & Role.STORAGED == Role.STORAGED):
-#            print >> sys.stdout, "%s" % __storaged_config_to_string(c)
-#        if (r & Role.ROZOFSMOUNT == Role.ROZOFSMOUNT):
-#            print >> sys.stdout, "%s" % __rozofsmount_config_to_string(c)
-#
-#
-# def config(platform, args):
-# #    print >> sys.stdout, "EXPORTD HOST: %s, PROTOCOL(S): %s" % (platform.get_exportd_hostname(), platform.get_sharing_protocols())
-#    configurations = platform.get_configurations(args.nodes, __args_to_roles(args))
-#    for h, c in configurations.items():
-#        __print_host_configs(h, c)
+    return s
 
-# def expand(platform, args):
-#    platform.add_nodes(args.hosts, args.vid)
-#
-#
-# def shrink(platform, args):
-#    for vid in args.vids:
-#        platform.remove_volume(vid)
-#
-#
-# def export(platform, args):
-#    platform.create_export(args.vid[0], args.name, args.passwd, args.squota, args.hquota)
-#
-#
-# def update(platform, args):
-#    platform.update_export(args.eid[0], args.current, args.passwd, args.squota, args.hquota)
-#
-#
-# def unexport(platform, args):
-#    if not args.eids:
-#        args.eids = None
-#
-#    platform.remove_export(args.eids, args.force)
-#
-#
-# def mount(platform, args):
-#    if not args.eids:
-#        args.eids = None
-#
-#    platform.mount_export(args.eids, args.nodes)
-#
-#
-# def umount(platform, args):
-#    if not args.eids:
-#        args.eids = None
-#
-#    platform.umount_export(args.eids, args.nodes)
 
+def __storaged_config_to_string(config):
+    s = "\t\t%-21s %-10s\n" % ('INTERFACE', 'PORT')
+    for lconfig in config.listens:
+        s += "\t\t%-21s %-10s\n" % (lconfig.addr, lconfig.port)
+    s += "\t\t%-10s %-10s %-30s\n" % ('CID', 'SID', 'ROOT')
+    keylist = config.storages.keys()
+    keylist.sort()
+    for key in keylist:
+        st = config.storages[key]
+        s += "\t\t%-10d %-10d %-30s\n" % (st.cid, st.sid, st.root)
+    return s
+
+
+def __rozofsmount_config_to_string(config):
+    s = "\t\t%-20s %-20s\n" % ('NODE', 'EXPORT')
+    for c in config:
+        s += "\t\t%-20s %-20s\n" % (c.export_host, c.export_path)
+    return s
+
+
+def __print_host_configs(host, configurations):
+    if configurations is not None and not configurations:
+        return
+
+    print >> sys.stdout, ":node:node status:roles:role statuses"
+    if configurations is None:
+        print >> sys.stdout, ":%s :%s:%s:%s" % (host, 'down', '', '')
+        return
+
+    # __double_line()
+    print >> sys.stdout, "NODE: %s - %s" % (host, 'UP')
+    for r, c in configurations.items():
+        # __single_line()
+        print >> sys.stdout, "\tROLE: %s" % ROLES_STR[r]
+        if (r & Role.EXPORTD == Role.EXPORTD):
+            print >> sys.stdout, "%s" % __exportd_config_to_string(c)
+        if (r & Role.STORAGED == Role.STORAGED):
+            print >> sys.stdout, "%s" % __storaged_config_to_string(c)
+        if (r & Role.ROZOFSMOUNT == Role.ROZOFSMOUNT):
+            print >> sys.stdout, "%s" % __rozofsmount_config_to_string(c)
+
+
+def config(platform, args):
+    if not args.roles:
+        args.roles = [EXPORTD_MANAGER, STORAGED_MANAGER, ROZOFSMOUNT_MANAGER]
+    configurations = platform.get_configurations(args.nodes, __args_to_roles(args))
+    host_l = {}
+    for h, c in configurations.items():
+        if c is not None and not c:
+            return
+
+        if c is None:
+            raise Exception ('%s is down.' % h)
+
+        role_l=[]
+        for role, config in c.items():
+            if (role & Role.EXPORTD == Role.EXPORTD):
+                exportd_l = []
+                volume_l = []
+                for v in config.volumes.values():
+                    cluster_l = []
+                    for cluster in v.clusters.values():
+                        s_l = []
+                        for s, hhh in cluster.storages.items():
+                            s_l.append({'sid ' + str(s): hhh})
+                    cluster_l.append({'cluster ' + str(cluster.cid): s_l})
+                    volume_l.append({'volume ' + str(v.vid): cluster_l})
+                exportd_l.append({'VOLUME':volume_l})
+                if len(config.exports) != 0:
+                    for e in config.exports.values():
+                        export_l = OrderedDict([
+                            ('vid', e.vid),
+                            ('root', e.root),
+                            ('md5', e.md5),
+                            ('squota', e.squota),
+                            ('hquota', e.hquota)])
+                        exportd_l.append({'EXPORT':export_l})
+                role_l.append({'EXPORTD':exportd_l})
+                            
+            if (role & Role.STORAGED == Role.STORAGED):
+                storage_l = []
+                interface_l = []
+                for lconfig in config.listens:
+                    interface_l.append({lconfig.addr : lconfig.port})
+                storage_l.append({'INTERFACE':interface_l})
+                keylist = config.storages.keys()
+                keylist.sort()
+                st_l = []
+                for key in keylist:
+                    st = config.storages[key]
+                    st_l.append({'cid ' + str(st.cid) + ', sid '+
+                        str(st.sid) : st.root})
+                storage_l.append({'STORAGE': st_l})
+                role_l.append({'STORAGED': storage_l})
+                    
+            if (role & Role.ROZOFSMOUNT == Role.ROZOFSMOUNT):
+                exp_l = []
+                for c in config:
+                    exp_l.append({'node ' +
+                        str(c.export_host) : c.export_path})
+                role_l.append({'ROZOFSMOUNT': exp_l})
+            
+            host_l.update({'NODE: ' + str(h) : role_l})
+
+    ordered_puts(host_l)
+    #__print_host_configs(h, c)
+        
 
 def dispatch(args):
     p = Platform(args.exportd)
