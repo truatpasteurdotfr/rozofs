9f7f6869ed6eb77c1ba41ac77bb1949f053c5cb4
rozodopamine <jean-pierre.monchanin@rozofs.org>
2014-11-10 17:33:57 +0100

    Fix issue #83 Meta-data server crash after unlink() rejection with No such file error

--- src/exportd/dirent_file.c
+++ src/exportd/dirent_file.c
@@ -1611,6 +1611,11 @@ int del_mdirentry(void *root_idx_bitmap_p,int dirfd, fid_t fid_parent, char * na
                     DIRENT_WARN(" ERROR  dirent_cache_release_entry\at line %d\n", __LINE__);
                 }
 
+                /*
+                 ** clear the pointer to the root entry
+                 */
+                root_entry_p = NULL;
+		
 #ifndef DIRENT_SKIP_DISK
                 int flags = 0;
                 int ret;
@@ -1620,10 +1625,7 @@ int del_mdirentry(void *root_idx_bitmap_p,int dirfd, fid_t fid_parent, char * na
                     goto out;
                 }
 #endif
-                /*
-                 ** clear the pointer to the root entry
-                 */
-                root_entry_p = NULL;
+
                 status = 0;
                 goto out;
             }
