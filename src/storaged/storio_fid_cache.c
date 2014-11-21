e1b4c36dfaa0f0fb7545de17de2cfce124958dea
rozodopamine <jean-pierre.monchanin@rozofs.org>
2014-11-21 13:32:50 +0100

    Change FID cache New files forgotten in previous commit

---
+++ src/storaged/storio_fid_cache.c
@@ -0,0 +1,471 @@
+/*
+ Copyright (c) 2010 Fizians SAS. <http://www.fizians.com>
+ This file is part of Rozofs.
+
+ Rozofs is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published
+ by the Free Software Foundation, version 2.
+
+ Rozofs is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see
+ <http://www.gnu.org/licenses/>.
+ */
+
+#define _XOPEN_SOURCE 500
+
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <limits.h>
+#include <unistd.h>
+#include <uuid/uuid.h>
+
+#include <rozofs/rozofs.h>
+#include <rozofs/common/log.h>
+#include <rozofs/common/xmalloc.h>
+
+#include "storio_fid_cache.h"
+
+STORIO_FID_CACHE_BUCKET_T   * storio_fid_cache;
+
+storio_fid_exact_match_fct    storio_fid_exact_match;
+storio_fid_delete_request_fct storio_fid_delete_request;
+
+STORIO_FID_STAT_T storio_fid_cache_stat;
+
+/*
+** lowest0 gives the rank of the lowest 0 in a 8bit value
+** lowest1 gives the rank of the lowest 1 in a 8bit value
+*/
+uint8_t lowest0[0xFF];
+uint8_t lowest1[0xFF];
+
+/*
+**______________________________________________________________________________
+** Display hash statistics
+**
+** @param pChar where to format the display
+** @retval The end of the display
+*/
+#define DISPLAY_CACHE_FID_STAT(x) pChar += sprintf (pChar,"cache %-6s: %llu\n", #x, (unsigned long long int)storio_fid_cache_stat.x);
+char * display_cache_fid_stat(char * pChar) {
+  DISPLAY_CACHE_FID_STAT(count);
+  DISPLAY_CACHE_FID_STAT(hit);
+  DISPLAY_CACHE_FID_STAT(miss);
+  DISPLAY_CACHE_FID_STAT(bkts);
+  DISPLAY_CACHE_FID_STAT(mxcol)
+  return pChar;
+}  
+/*
+**______________________________________________________________________________
+** Initializae the lowest0 and lowest1 tables
+** lowest0 gives the rank of the lowest 0 in a 8bit value
+** lowest1 gives the rank of the lowest 1 in a 8bit value
+*/
+void storio_fid_cache_init_lowest_arrays() {
+  uint32_t    val;
+  uint8_t    bit;
+  
+  for (val = 0; val < 0x100; val++) {
+  
+    /*
+    ** Look for lowest 1 in val 
+    */
+    for (bit=0; bit<8; bit++) {
+      if (val & (1<<bit)) {
+        lowest1[val] = bit;
+	break;
+      }
+    }
+    
+    /*
+    ** Look for lowest 0 in val 
+    */
+    for (bit=0; bit<8; bit++) {
+      if (!(val & (1<<bit))) {
+        lowest0[val] = bit;
+	break;
+      }
+    }    
+    
+  }
+}
+/*
+/*
+**______________________________________________________________________________
+** Insert a FID context reference in the cache 
+** 
+** @param hash   A 32 bits hash value of the entry
+** @param index  The index to store
+**
+** @retval 0  on success
+** @retval -1 on error 
+*/
+int storio_fid_cache_insert(uint32_t hash, uint32_t index) {
+  STORIO_FID_CACHE_BUCKET_T     * pBucket;
+  STORIO_FID_CACHE_SUB_BUCKET_T * pSub;
+  STORIO_FID_CACHE_ENTRY_U      * pEntry;
+  int                        bbyte, bbit;
+  int                        sbyte, sbit;
+  int                        subIdx;
+  int                        entryIdx;
+  int                        idx;
+
+  /* 
+  ** Get the bucket entry from the hash value
+  */
+  pBucket = storio_fid_cache;
+  pBucket += (hash & ((1<<STORIO_FID_CACHE_LVL0_SZ_POWER_OF_2)-1));
+
+  /*
+  ** Keep upper bits for the entry hash in the bucket
+  */  
+  hash = (hash >> STORIO_FID_CACHE_LVL0_SZ_POWER_OF_2) & ((1<<STORIO_FID_CACHE_HASH_BITS)-1);
+  
+ 
+restart:  
+  /*
+  ** Find the 1rst not full sub bucket 
+  */
+  for (bbyte=0; bbyte < STORIO_FID_CACHE_MAX_SUB_BUCKET_BITMAP; bbyte++) {
+    if (pBucket->full[bbyte] != 0xFF) break;
+  }
+  if (bbyte == STORIO_FID_CACHE_MAX_SUB_BUCKET_BITMAP) {
+    /*
+    ** All sub buckets are full. Should free an entry
+    */
+    for (bbyte=0; bbyte < STORIO_FID_CACHE_MAX_SUB_BUCKET_BITMAP; bbyte++) {
+      for (bbit=0; bbit< 8; bbit++) {
+      
+        subIdx = 8 * bbyte + bbit;
+	if (subIdx==0) pSub = &pBucket->bucket0;
+        else           pSub = pBucket->xtra_bucket[subIdx-1]; 
+
+        for (sbyte=0; sbyte < STORIO_FID_CACHE_MAX_SUB_BUCKET_ENTRIES_BITMAP; sbyte++) {
+          for (sbit=0; sbit< 8; sbit++) {
+            entryIdx = 8 * sbyte + sbit;
+            pEntry = &pSub->entry[entryIdx]; 
+	    if (storio_fid_delete_request(pEntry->s.index)) break;
+	  }
+	  if (sbit != 8) break;
+	}
+	if (sbyte != STORIO_FID_CACHE_MAX_SUB_BUCKET_ENTRIES_BITMAP) break;
+      }
+      if (bbit != 8) break;
+    }  	  
+  }
+  if (bbyte == STORIO_FID_CACHE_MAX_SUB_BUCKET_BITMAP) {
+    /*
+    ** Bucket full and no entry freed !!! 
+    */
+    severe("bucket full");
+    return -1;
+  }
+  
+  /*
+  ** Get 1rst not full sub bucket
+  */
+  bbit = lowest0[pBucket->full[bbyte]];
+  subIdx = 8 * bbyte + bbit;
+  if (subIdx==0) pSub = &pBucket->bucket0;
+  else           pSub = pBucket->xtra_bucket[subIdx-1]; 
+  
+  /*
+  ** Allocate the sub bucket if needed
+  */
+  if (pSub == 0) {
+  
+    pSub = malloc(sizeof(STORIO_FID_CACHE_SUB_BUCKET_T));
+    pBucket->xtra_bucket[subIdx-1] = pSub;
+    if (pSub == NULL) {
+      severe("Out of memory");
+      return -1;
+    }
+    storio_fid_cache_stat.bkts++;
+    memset(pSub,0,sizeof(STORIO_FID_CACHE_SUB_BUCKET_T));  
+  }
+  
+    
+  /*
+  ** Find a free entry in the sub bucket
+  */
+  for (sbyte=0; sbyte < STORIO_FID_CACHE_MAX_SUB_BUCKET_ENTRIES_BITMAP; sbyte++) {
+    if (pSub->allocated[sbyte] != 0xFF) break;
+  }
+  if (sbyte == STORIO_FID_CACHE_MAX_SUB_BUCKET_ENTRIES_BITMAP) {
+    /*
+    ** Sub bucket was not indicated as full !!!
+    */
+    severe("sub bucket full");
+    pBucket->full[bbyte] |= (1<<bbit);
+    pBucket->empty[bbyte] &= ~(1<<bbit);    
+    goto restart;
+  }   
+  
+  /*
+  ** Get 1rst free entry
+  */
+  sbit = lowest0[pSub->allocated[sbyte]];
+  entryIdx = 8 * sbyte + sbit;
+  pEntry = &pSub->entry[entryIdx]; 
+  
+  /*
+  ** Write entry
+  */
+  pEntry->s.hash  = hash;
+  pEntry->s.index = index; 
+  
+  
+  /*
+  ** Update sub subcket bit map
+  */
+  pSub->allocated[sbyte] |= (1<<sbit);
+  
+  /*
+  ** Is the sub bucket full
+  */
+  for (idx=0; idx < STORIO_FID_CACHE_MAX_SUB_BUCKET_ENTRIES_BITMAP; idx++) {
+    if (pSub->allocated[idx] != 0xFF) break;
+  }
+  if (idx == STORIO_FID_CACHE_MAX_SUB_BUCKET_ENTRIES_BITMAP) {
+    pBucket->full[bbyte] |= (1<<bbit);
+  }  
+  
+  /* 
+  ** Sub bucket is not empty
+  */
+  if (pBucket->empty[bbyte] & (1<<bbit)) {
+    pBucket->empty[bbyte] &= ~(1<<bbit);
+  }
+  storio_fid_cache_stat.count++;
+  return 0;
+}
+/*
+**______________________________________________________________________________
+** Search or remove a FID context reference in the cache 
+** 
+** @param hash   A 32 bits hash value of the search entry
+** @param key    Pointer to the key for exact match 
+** @param search Whether it is a search or remove
+**
+** @retval    The index found in the cache
+** @retval    -1 when no entry found
+*/
+static inline uint32_t storio_fid_cache_searchOrRemove(uint32_t hash, void * key, int search) {
+  STORIO_FID_CACHE_BUCKET_T     * pBucket;
+  STORIO_FID_CACHE_SUB_BUCKET_T * pSub;
+  STORIO_FID_CACHE_ENTRY_U      * pEntry;
+  int                        bbyte, bbit;
+  int                        sbyte, sbit;
+  int                        subIdx;
+  int                        entryIdx;
+  uint8_t                    valb;
+  uint8_t                    vals;
+  int                        collision=0;
+  uint32_t                   retval=-1;
+
+  /* 
+  ** Get the bucket entry from the hash value
+  */
+  pBucket = storio_fid_cache;
+  pBucket += (hash & ((1<<STORIO_FID_CACHE_LVL0_SZ_POWER_OF_2)-1));
+  
+  /*
+  ** Keep upper bits for the entry hash in the bucket
+  */  
+  hash = (hash >> STORIO_FID_CACHE_LVL0_SZ_POWER_OF_2) & ((1<<STORIO_FID_CACHE_HASH_BITS)-1);
+  
+  /*
+  ** look for the entry in not empty sub buckets 
+  */
+  bbyte = 0;
+  for (;bbyte < STORIO_FID_CACHE_MAX_SUB_BUCKET_BITMAP; bbyte++) {
+  
+    valb = pBucket->empty[bbyte];
+    while (valb != 0xFF) {
+    
+      /*
+      ** Get 1rst non empty entry and
+      ** set this sub bucket as processed for next loop
+      */
+      bbit = lowest0[valb];
+      valb |= (1<<bbit); 
+      
+      /*
+      ** Get sub bucket entry
+      */
+      subIdx = 8 * bbyte + bbit;
+      if (subIdx==0) pSub = &pBucket->bucket0;
+      else           pSub = pBucket->xtra_bucket[subIdx-1]; 
+      if (pSub == 0) {
+        severe("Empty sub bucket");
+	pBucket->empty[bbyte] &= (1<<bbit);
+        continue;
+      }	 	
+  
+      /*
+      ** Check for allocated entries in the sub bucket
+      */
+      for (sbyte=0; sbyte < STORIO_FID_CACHE_MAX_SUB_BUCKET_ENTRIES_BITMAP; sbyte++) {
+      
+        vals = pSub->allocated[sbyte];
+	while (vals != 0) {
+
+	  /*
+	  ** Get 1rst allocated entry and
+	  ** set this entry as processed for next loop
+	  */
+	  sbit = lowest1[vals];
+	  vals &= ~(1<<sbit);
+
+	  /*
+	  ** Get entry and compare the hash value
+	  */
+	  entryIdx = 8 * sbyte + sbit;
+	  pEntry = &pSub->entry[entryIdx];
+	  if (pEntry->s.hash != hash) {
+	    continue;
+	  }  
+
+	  /*
+	  ** Hash match so compare the total key value
+	  */
+	  if (!storio_fid_exact_match(key,pEntry->s.index)) { 
+	    collision++;
+            if (collision>storio_fid_cache_stat.mxcol) {
+	      storio_fid_cache_stat.mxcol = collision;
+	    }	    
+            continue;
+	  }
+	  	  
+	  storio_fid_cache_stat.hit++;	  
+	  retval = pEntry->s.index;
+	  
+	  /*
+	  ** It is finished for the case of the search
+	  */
+	  if (search) {
+	    return retval;
+	  }
+
+	  /* 
+	  ** Case of the remove
+	  */
+	  pSub->allocated[sbyte] &= ~(1<<sbit);
+          storio_fid_cache_stat.count--;
+
+	  /* 
+	  ** Is the sub bucket empty now
+	  */
+	  int idx;
+          for (idx=0; idx < STORIO_FID_CACHE_MAX_SUB_BUCKET_ENTRIES_BITMAP; idx++) {
+            if (pSub->allocated[idx] != 0) break;
+          }
+	  if (idx == STORIO_FID_CACHE_MAX_SUB_BUCKET_ENTRIES_BITMAP) {
+	    /*
+	    ** Bucket is empty now
+	    */
+	    pBucket->empty[bbyte] |= (1<<bbit);
+	    /*
+	    ** Free sub bucket. Except the 1rst
+	    */
+	    if (subIdx != 0) { 
+	      free(pSub);
+	      storio_fid_cache_stat.bkts--;
+	      pBucket->xtra_bucket[subIdx-1] = NULL;
+	    }
+	  }  
+
+	  /* 
+	  ** Sub bucket is not full
+	  */
+	  if (pBucket->full[bbyte] & (1<<bbit)) {
+	    pBucket->full[bbyte] &= ~(1<<bbit);
+	  }
+	  
+	  return retval;
+	}
+      }
+    }
+  }
+  
+  storio_fid_cache_stat.miss++;
+  return retval;
+}
+/*
+**______________________________________________________________________________
+** Search a FID context reference in the cache 
+** 
+** @param hash   A 32 bits hash value of the search entry
+** @param key    Pointer to the key for exact match 
+**
+** @retval    The index found in the cache
+** @retval    -1 when no entry found
+*/
+uint32_t storio_fid_cache_search(uint32_t hash, void * key) {
+ return storio_fid_cache_searchOrRemove(hash, key, 1);
+}
+/*
+/*
+**______________________________________________________________________________
+** Remove a FID context reference from the cache 
+** 
+** @param hash   A 32 bits hash value of the entry to remove
+** @param key    Pointer to the key for exact match 
+**
+** @retval    The index of the removed entry
+** @retval    -1 when no entry found
+*/
+uint32_t storio_fid_cache_remove(uint32_t hash, void * key) {
+ return storio_fid_cache_searchOrRemove(hash, key, 0);
+}
+/*
+**______________________________________________________________________________
+** Cache initialization 
+** 
+** @param exact_match_fct     A function to be called for checking the exact match
+** @param delete_request_fct  A function to be called to request a context deletion
+**                            if possible
+**
+*/
+void storio_fid_cache_init(storio_fid_exact_match_fct exact_match_fct, storio_fid_delete_request_fct delete_request_fct) {
+  int count = (1<<STORIO_FID_CACHE_LVL0_SZ_POWER_OF_2);
+  int size  = sizeof(STORIO_FID_CACHE_BUCKET_T) * count;
+  STORIO_FID_CACHE_BUCKET_T     * pBucket;
+  int  i,j;
+
+  /*
+  ** Store exact match and delete request functions
+  */
+  storio_fid_exact_match = exact_match_fct;
+  storio_fid_delete_request = delete_request_fct;
+
+  storio_fid_cache_init_lowest_arrays();
+
+  /*
+  ** Allocate cache table
+  */
+  storio_fid_cache = malloc(size);
+  if (storio_fid_cache == NULL) {
+    fatal(" out of memory %d", size);   
+  }
+  memset(storio_fid_cache,0,size);
+
+  /*
+  ** init of the buckets
+  */
+  pBucket = storio_fid_cache;
+  for (i = 0; i < count; i++, pBucket++) {
+    for (j=0; j < STORIO_FID_CACHE_MAX_SUB_BUCKET_BITMAP; j++) {
+      pBucket->empty[j] = 0xFF;
+    }
+  }
+}
---
+++ src/storaged/storio_fid_cache.h
@@ -0,0 +1,191 @@
+/*
+ Copyright (c) 2010 Fizians SAS. <http://www.fizians.com>
+ This file is part of Rozofs.
+
+ Rozofs is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published
+ by the Free Software Foundation, version 2.
+
+ Rozofs is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see
+ <http://www.gnu.org/licenses/>.
+ */
+ 
+#ifndef STORIO_FID_CACHE_H
+#define STORIO_FID_CACHE_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif /*__cplusplus*/
+
+
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <limits.h>
+#include <unistd.h>
+#include <uuid/uuid.h>
+
+#include <rozofs/rozofs.h>
+#include <rozofs/common/log.h>
+#include <rozofs/common/list.h>
+#include <rozofs/common/xmalloc.h>
+#include <rozofs/common/profile.h>
+#include <rozofs/common/mattr.h>
+#include <rozofs/core/com_cache.h>
+#include <rozofs/core/ruc_list.h>
+
+/*
+**
+** cache->bucket[]---> empty sub bucket bitmap
+**                     full sub bucket bitmap
+**                     sub_bucket[]----------------> allocated entry bitmap
+**                                                   entry|]
+**
+*/
+
+
+/**
+** Exact match function
+** @param key      The context key (i.e FID)
+** @param index    The context index
+** 
+** @retval true in case of a match
+*/
+typedef uint32_t (*storio_fid_exact_match_fct)(void * key,uint32_t index);
+/**
+** Request from the cache to free a context if possible
+**
+** @param index    The context index
+** 
+** @retval true in case the context has been deleted
+*/
+typedef uint32_t (*storio_fid_delete_request_fct)(uint32_t index);
+
+
+/*
+** Number of cache entries in power of 2 
+*/
+#define  STORIO_FID_CACHE_LVL0_SZ_POWER_OF_2 12
+
+/*
+** In an 32 bit is stored the index as well as a hash
+** The hash size STORIO_FID_CACHE_HASH_BITS 
+*/
+#define STORIO_FID_CACHE_HASH_BITS 14
+#define STORIO_INDEX_BITS (32-STORIO_FID_CACHE_HASH_BITS)
+typedef union storio_fid_cache_entry_u {
+  uint32_t      u32;
+  struct {
+    uint32_t    hash:STORIO_FID_CACHE_HASH_BITS;
+    uint32_t    index:STORIO_INDEX_BITS;
+  } s;
+} STORIO_FID_CACHE_ENTRY_U;
+
+
+/*
+** Each sub bucket contains a bit map of the used entries
+** as well as the array of entries
+*/
+#define STORIO_FID_CACHE_MAX_SUB_BUCKET_ENTRIES     32
+#define STORIO_FID_CACHE_MAX_SUB_BUCKET_ENTRIES_BITMAP (STORIO_FID_CACHE_MAX_SUB_BUCKET_ENTRIES/8)
+typedef struct storio_fid_cache_sub_bucket_t {
+  /* Bit map of allocated entries in the buket */
+  uint8_t                             allocated[STORIO_FID_CACHE_MAX_SUB_BUCKET_ENTRIES_BITMAP];
+  STORIO_FID_CACHE_ENTRY_U            entry[STORIO_FID_CACHE_MAX_SUB_BUCKET_ENTRIES];  
+} STORIO_FID_CACHE_SUB_BUCKET_T;
+
+/*
+** A bucket is a collection of sub buckets containing entry data
+** A bitmap gives the list of empty sub buckets where the search needs not to read.
+** An other gives the list of full sub buckets where the insert needs not to read.
+** The 1rst sub bucket is in the bucket conext, while extra sub buckets are allocated
+** and released on usage.
+*/
+#define STORIO_FID_CACHE_MAX_SUB_BUCKET     32
+#define STORIO_FID_CACHE_MAX_SUB_BUCKET_BITMAP (STORIO_FID_CACHE_MAX_SUB_BUCKET/8)
+typedef struct storio_fid_cache_bucket_t {
+  /* Bit map of the empty sub-buckets */
+  uint8_t       empty[STORIO_FID_CACHE_MAX_SUB_BUCKET_BITMAP];
+  /* Bit map of the full sub-buckets */  
+  uint8_t       full[STORIO_FID_CACHE_MAX_SUB_BUCKET_BITMAP];
+  /* 1rst bucket */
+  STORIO_FID_CACHE_SUB_BUCKET_T   bucket0;
+  /* Extra Sub buckets address */
+  STORIO_FID_CACHE_SUB_BUCKET_T * xtra_bucket[STORIO_FID_CACHE_MAX_SUB_BUCKET-1];
+} STORIO_FID_CACHE_BUCKET_T;
+
+
+/*
+** Cache statistics
+*/
+typedef struct storio_fid_stat_t {
+  uint64_t        count;   // nb of entries in the cache
+  uint64_t        hit;     // count of successfull search
+  uint64_t        miss;    // count of failed search
+  uint64_t        bkts;    // number of allocated extra buckets
+  uint64_t        mxcol;   // max collision found on entry hash & bucket index
+} STORIO_FID_STAT_T;
+
+
+/*
+**______________________________________________________________________________
+** Display hash statistics
+**
+** @param pChar where to format the display
+** @retval The end of the display
+*/
+char * display_cache_fid_stat(char * pChar);
+/*
+**______________________________________________________________________________
+** Insert a FID context reference in the cache 
+** 
+** @param hash   A 32 bits hash value of the entry
+** @param index  The index to store
+**
+** @retval 0  on success
+** @retval -1 on error 
+*/
+int storio_fid_cache_insert(uint32_t hash, uint32_t index) ;
+/*
+**______________________________________________________________________________
+** Search a FID context reference in the cache 
+** 
+** @param hash   A 32 bits hash value of the searched entry
+** @param key    Pointer to the key for exact match 
+**
+** @retval    The index found in the cache
+** @retval    -1 when no entry found
+*/
+uint32_t storio_fid_cache_search(uint32_t hash, void * key) ;
+/*
+**______________________________________________________________________________
+** Remove a FID context reference from the cache 
+** 
+** @param hash   A 32 bits hash value of the entry to remove
+** @param key    Pointer to the key for exact match 
+**
+** @retval    The index of the removed entry
+** @retval    -1 when no entry found
+*/
+uint32_t storio_fid_cache_remove(uint32_t hash, void * key) ;
+/*
+**______________________________________________________________________________
+** Cache initialization 
+** 
+** @param exact_match_fct     A function to be called for checking the exact match
+** @param delete_request_fct  A function to be called to request a context deletion
+**                            if possible
+**
+*/
+void storio_fid_cache_init(storio_fid_exact_match_fct exact_match_fct, storio_fid_delete_request_fct delete_request_fct) ;
+#endif
